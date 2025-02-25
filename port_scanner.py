dc81aa2fc83fd795fe45c591e07b6b830405b35c
mbeardwell <matthewbeardwell@gmail.com>
2025-02-25 00:20:46 +0000

    Added support for a single argument - ipv4_address

--- port_scanner.py
+++ port_scanner.py
@@ -1,1 +1,30 @@
+#!/bin/python3
+import argparse
+import re
+import socket
 
+# Setup Argparse to parse input strings.
+
+parser = argparse.ArgumentParser(
+	prog='port_scanner.py',
+	description='Scans an IPv4 address for open ports.')
+
+parser.add_argument('ipv4_address')
+
+def process_ipv4_from_args(ipv4_address):
+	regex = r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$"
+	ipv4_num_args = 4
+	regex_match = re.match(regex, ipv4_address)
+	if regex_match is None:
+		raise argparse.ArgumentTypeError(f"Invalid format for IPv4 address: {ipv4_address}")
+	else:
+		try:
+			values = [int(regex_match.group(i)) for i in range(1,ipv4_num_args+1)]
+			for v in values:
+				if not (0 <= v <= 255):
+					raise ValueError(v)
+		except:
+			raise argparse.ArgumentTypeError(f"IPv4 address not in the correct range: {ipv4_address}")
+	return values
+
+args = parser.parse_args()
