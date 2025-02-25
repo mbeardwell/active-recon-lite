1fd24f58a5c284386e01a17fe9360f3e40c948e8
mbeardwell <matthewbeardwell@gmail.com>
2025-02-25 02:07:53 +0000

    Implemented TCP scanning.

--- port_scanner.py
+++ port_scanner.py
@@ -3,15 +3,8 @@ import argparse
 import re
 import socket
 
-# Setup Argparse to parse input strings.
-
-parser = argparse.ArgumentParser(
-	prog='port_scanner.py',
-	description='Scans an IPv4 address for open ports.')
-
-parser.add_argument('ipv4_address')
-
-def process_ipv4_from_args(ipv4_address):
+# Validates the input argument IPv4 address.
+def validate_ipv4_address(ipv4_address):
 	regex = r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$"
 	ipv4_num_args = 4
 	regex_match = re.match(regex, ipv4_address)
@@ -25,6 +18,31 @@ def process_ipv4_from_args(ipv4_address):
 					raise ValueError(v)
 		except:
 			raise argparse.ArgumentTypeError(f"IPv4 address not in the correct range: {ipv4_address}")
-	return values
+	return
 
+# Setup Argparse to parse input strings.
+parser = argparse.ArgumentParser(
+	prog='port_scanner.py',
+	description='Scans an IPv4 address for open ports.')
+
+parser.add_argument('ipv4_address')
+
+# Read in the IPv4 address.
 args = parser.parse_args()
+
+# Scan TCP ports
+PORT_MAX = 2**16 - 1
+PORT_MIN = 1
+open_tcp_ports = []
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+	for port in range(PORT_MIN, PORT_MAX + 1):
+		try:
+			sock.connect((args.ipv4_address, port))
+			open_tcp_ports.append(port)
+			print(f"Open TCP port: {port}")
+		except:
+			pass
+		sock.close()
+
+if len(open_tcp_ports) == 0:
+	print("No open TCP ports.")
